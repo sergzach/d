384a7b63973bec18afcf046b414a9bd83b9b5f35
Sergey <sergzach@yandex.ru>
2024-05-19 12:40:10 +0300

    Create __init__.py

---
+++ d/__init__.py
@@ -0,0 +1,47 @@
+import importlib
+
+
+def reload(module):
+    '''
+    Use the function to reload a current mod.
+    '''
+    importlib.reload(module)
+
+
+def patch_cls(
+        *,
+        patching_module,
+        user_patching_module,
+        class_name,
+        method_name
+):
+    def create_debug_patched_method():
+        def inner(*args, **kwargs):
+            while True:
+                try:
+                    getattr(user_patching_module, 'user_patch')(patching_module)
+                    getattr(user_patching_module, f'{method_name}')(*args, **kwargs)
+                except Exception as e:
+                    getattr(user_patching_module, 'on_exception')(e)
+                finally:
+                    if init.ask_repeat:
+                        ans = input('Do repeat (y/any for no)? ')
+                        if ans != 'y':
+                            print(f'Debugging of {method_name} is finished.')
+                            break
+        return inner
+
+    reload(user_patching_module)
+
+    cls_ = getattr(patching_module, class_name)
+
+    setattr(
+        cls_,
+        method_name,
+        create_debug_patched_method()
+    )
+
+
+def init(*, patching_module, user_patch_module, ask_repeat=True):
+    init.ask_repeat = ask_repeat
+    getattr(user_patch_module, 'user_patch')(patching_module)
