a059535e216ea7d18da637be3aadc14c3e30fa96
Sergey <sergzach@yandex.ru>
2024-05-19 12:39:10 +0300

    Create example.py

---
+++ examples/example.py
@@ -0,0 +1,40 @@
+
+import sys
+
+import d
+import d_example
+
+
+def bold(substring):
+    return f'\033[1m{substring}\033[0m'
+
+
+class TestClass:
+    def member(self):
+        print('In member function.')
+
+    def foo(self, k):
+        print(k)
+        print(f'Replace the function in TestClass with {bold("d.")}')
+
+
+def foo(k):
+    print(k)
+    print(f'Replace the global function with {bold("d.")}')
+
+
+def main():
+    d.init(
+        patching_module=sys.modules[__name__],
+        user_patch_module=d_example,
+        ask_repeat=True
+    )
+
+    test_class = TestClass()
+    test_class.foo(25)
+    test_class.foo(26)
+    #foo(26)
+
+
+if __name__ == '__main__':
+    main()
